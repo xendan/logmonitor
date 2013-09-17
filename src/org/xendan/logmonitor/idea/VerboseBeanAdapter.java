1acf93744f2d8efab0574d7eb41f97603a375f9f
xendan <mukhametova@gmail.com>
2013-09-17 17:16:08 +0200

    fix test

--- src/org/xendan/logmonitor/idea/VerboseBeanAdapter.java
+++ src/org/xendan/logmonitor/idea/VerboseBeanAdapter.java
@@ -4,12 +4,16 @@ import com.jgoodies.binding.beans.BeanAdapter;
 import com.jgoodies.binding.value.AbstractValueModel;
 import com.jgoodies.binding.value.ValueModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: id967161
  * Date: 17/09/13
  */
 public class VerboseBeanAdapter<T>  {
     private final BeanAdapter<T> adapter;
+    private List<VerboseModel> models = new ArrayList<VerboseModel>();
 
     public VerboseBeanAdapter(T bean) {
         adapter = new BeanAdapter<T>(bean);
@@ -20,11 +24,16 @@ public class VerboseBeanAdapter<T>  {
     }
 
     public ValueModel getPropertyModel(String property) {
-        return new VerboseModel(adapter.getValueModel(property));
+        VerboseModel model = new VerboseModel(adapter.getValueModel(property));
+        models.add(model);
+        return model;
     }
 
     public void setBean(T newBean) {
         adapter.setBean(newBean);
+        for (VerboseModel model : models) {
+            model.fireChanged(model.getValue(), null);
+        }
     }
 
     private class VerboseModel extends AbstractValueModel {
@@ -43,6 +52,10 @@ public class VerboseBeanAdapter<T>  {
         public void setValue(Object newValue) {
             Object oldValue = getValue();
             valueModel.setValue(newValue);
+            fireChanged(newValue, oldValue);
+        }
+
+        private void fireChanged(Object newValue, Object oldValue) {
             firePropertyChange("value", oldValue, newValue);
         }
     }
